053a128c7680f374fad6f308ab55a90fee393aea
kfeng123 <446100240@qq.com>
2015-07-11 23:32:55 +0800

    7_11

--- season2/offlineAnalysis/offlineAnalysis.R
+++ season2/offlineAnalysis/offlineAnalysis.R
@@ -1,4 +1,5 @@
 library("forecast")
+library("moments")
 myTot=read.csv("myTot.csv",header=FALSE)
 myTot=myTot[,-4]
 names(myTot)=c("report_date","purchase","redeem")
@@ -25,6 +26,9 @@ buxiu=myTot$report_date %in% buxiutemp+0
 #周末
 xingqi=strftime(myTot$report_date,format="%w")
 zhoumo=as.numeric(xingqi%in%c("0","6"))
+
+#月初，每月1号
+yuechu=(strftime(myTot$report_date,format="%d")=="01")+0
 #########################
 
 
@@ -37,34 +41,49 @@ points(1:427,(myTot$purchase*jiaqi),type="p",col="red")
 points(1:427,(myTot$purchase*buxiu),type="p",col="green")       
 
 
-
-
-
 sx=ts(myTot$purchase,frequency=7,start=c(1,1))
-#autoFit=auto.arima(sx,d=0,D=1,trace=TRUE)
-#plot(forecast(autoFit,h=30)$residual)
-##points((forecast(autoFit,h=30)$residual*jiaqi*(1-zhoumo)),type="p")
-#points(forecast(autoFit,h=30)$residual*jiaqi*zhoumo,type="p",col="blue")
-#points(forecast(autoFit,h=30)$residual*buxiu,type="p",col="red")
-myStl=stl(sx,s.window="periodic",robust=TRUE)
-plot(forecast(myStl,h=30)$mean)
+
+xingqi=NULL
+for(i in 1:7){
+        xingqi[[i]]=sx[cycle(sx)==i]
+}
+for(i in 1:7){
+        plot(xingqi[[i]],type="o",main=i)
+}
+for(i in 1:7){
+        plot(forecast(auto.arima(xingqi[[i]]),h=30))
+}
+
+
+#研究残差
+##相对误差
+myD=data.frame(purchase=sx,seasonaldummy(sx))
+myD=myD[275:427,]
+lmFit=lm(purchase~S1+S2+S3+S4+S5+S6,data=myD)
+temp=ts(1:7,frequency=7,start=c(1,1))
+temp=seasonaldummy(temp)
+predict(lmFit,data.frame(temp))
+hist(residuals(lmFit))
+hist(residuals(lmFit)/fitted.values(lmFit))
+plot(residuals(lmFit),type="l")
+plot(residuals(lmFit)/fitted.values(lmFit),type="l")
+temp=(strftime(myTot$report_date,format="%d")=="01")
+temp2=(1:427)[temp]
+abline(v=temp2-274)
+points((residuals(lmFit)/fitted.values(lmFit)*zhoumo[275:427]),type="p")
+points((residuals(lmFit)/fitted.values(lmFit)*jiaqi[275:427]),type="p",col="red")
+points((residuals(lmFit)/fitted.values(lmFit)*buxiu[275:427]),type="p",col="green")       
 
 
 
 
 
+myD=data.frame(purchase=sx,seasonaldummy(sx),yuechu,jiaqi,buxiu)
+myD=myD[275:427,]
+lmFit=lm(purchase~.,myD)
 
+temp=ts(1:30,frequency=7,start=c(1,1))
+Pseason=seasonaldummy(temp)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+toP=data.frame(Pseason,yuechu=(1:30%in%1) +0,jiaqi=(1:30%in%8) +0,buxiu=(1:30%in%28) +0)
+plot(predict(lmFit,toP),type="o")
